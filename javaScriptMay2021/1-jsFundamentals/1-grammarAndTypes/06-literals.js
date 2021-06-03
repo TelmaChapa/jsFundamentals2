65673e24430e7d730c7bf814a86bce98e02cf1ba
Telma Chapa <telchapa@gmail.com>
2021-06-03 17:53:25 -0400

    We are goint to upload our code to github

---
+++ javaScriptMay2021/1-jsFundamentals/1-grammarAndTypes/06-literals.js
@@ -0,0 +1,33 @@
+/*
+LITERALS
+
+1-grammarAndTypes
+  06-literals.js
+*/
+
+let weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+//                 0          1           2            3          4
+console.log(weekDays.toString());
+console.log(weekDays[3]);
+
+let soup = {
+  a: 'chicken noodle',
+  b: 'tomato',
+  c: 'beefand barley'  
+}
+console.log(soup.c);
+
+//array and object literals can hold primitive and 'complex' types (arrays, object, functions)
+let complexArrLiteral = [1, true,'Academy', [1,2,3,], {key: 'test'}, function(){return 'yes'}];
+//                        0   1      2          3            4          5
+console.log(complexArrLiteral[5]);
+console.log(complexArrLiteral[3]);
+
+let complexObjLiteral = {num: 1, boolean: true, string: 'Academy', arr: [1,2,3], obj: {key: 'test'}, func: function(){return 'yes'}};
+
+console.log(complexObjLiteral.func);
+console.log(complexObjLiteral.arr);
+//My searches, i was curious to see the returns
+console.log(complexObjLiteral.string);
+console.log(complexObjLiteral.obj);
+console.log(complexObjLiteral.num);
